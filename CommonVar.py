e8562e67d6e5ba12e3b60654d0f9e49db0d610ed
wsligor <wsligor@gmail.com>
2024-10-25 09:22:54 +0300

    ## *Релиз v1.3.2* 1. Файловая оптимизация

--- CommonVar.py
+++
@@ -1,9 +0,0 @@
-import os
-
-appDir = os.getcwd()
-tmpDir = f'{appDir}/tmp/'
-# source_filename = self.fileName
-tempDir = f'{appDir}\\temp\\'
-imgTempDir = f'{appDir}\\temp\\img\\'
-# print(f'source_filename= {source_filename}')
-# print(f'dest_filename = {tempDir}')
--- training.py
+++
@@ -1,9 +0,0 @@
-import config
-
-
-def checking_party_input():
-    return config.CHECKING_PARTY_INPUT and config.ACCESS_PRINTER == 'NETWORK'
-
-
-if __name__ == '__main__':
-    print(config.VERSION)
